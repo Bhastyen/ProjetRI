9c460d3fd2c2e3b6e2ac5d5015e2c1dfb798e3b6
Laetitia <testhyl@hotmail.com>
2019-10-30 15:30:48 +0100

    Creation Class Document

---
+++ src/Document.java
@@ -0,0 +1,26 @@
+
+public class Document {
+	int idDoc;
+	String stringDocument;
+	
+	public Document(int idDoc, String stringDocument) {
+		super();
+		this.idDoc = idDoc;
+		this.stringDocument = stringDocument;
+	}
+	
+	public int getIdDoc() {
+		return idDoc;
+	}
+	public void setIdDoc(int idDoc) {
+		this.idDoc = idDoc;
+	}
+	public String getStringDocument() {
+		return stringDocument;
+	}
+	public void setStringDocument(String stringDocument) {
+		this.stringDocument = stringDocument;
+	}
+	
+	
+}
