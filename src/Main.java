8b6d7db52569a231bd4ac5b91dd05d4c78f0ece2
Bhastyen <bastien.duraj@live.fr>
2019-10-30 16:28:23 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 
 public class Main {
 
+	
+	// function : parserDoc(String pathResources, String type) output(List<Document>)  , type = "xml" ou "brut"
+	
 	public static void main(String[] args) {
 		System.out.println("C'est ok");
 	}
