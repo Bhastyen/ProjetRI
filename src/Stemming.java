4fe4993cdf009c9c2cb58574384dbb51b68db9d3
Bhastyen <bastien.duraj@live.fr>
2019-11-12 20:27:49 +0100

    Import librairie open nlp + correction classe Stemming.

--- src/Stemming.java
+++ src/Stemming.java
@@ -1,14 +1,13 @@
-import opennlp.tools.stemmer.PorterStemmer.stem;
+import opennlp.tools.stemmer.PorterStemmer;
+
 
 public class Stemming{
 	
-		public static void main(String[] args) {
-
-			System.out.println(stemTerm(  "annoying"  ));
+	public static void main(String[] args) {
+		System.out.println(stemTerm("annoying"));
 	}
 	
 	public static String stemTerm(String term){
-		  return stem(term);
-		}
-	
+		return new PorterStemmer().stem(term);
+	}
 }
